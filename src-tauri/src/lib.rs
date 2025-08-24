99951c2dd2dfbf5b9c22b9662f5a07eb41be0666
Ricardo Teixeira <ricardoteixeirasantos@hotmail.com>
2025-08-23 23:56:30 -0300

    feat: :zap: Single connection to wb and pool across multiple chats

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,27 +1,59 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![warn(unused_crate_dependencies)]
 
-use std::collections::HashMap;
-use std::sync::Mutex;
 use rustls as _;
+use sqlx::SqlitePool; // Importe o SqlitePool
+use std::sync::Arc;
+use tauri_plugin_sql::{Migration, MigrationKind};
+use tokio::sync::Mutex;
+use tauri::{Manager};
 
 mod commands;
 mod models;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            sql: "CREATE TABLE IF NOT EXISTS senders (id INTEGER PRIMARY KEY, username TEXT,slug TEXT, color TEXT);",
+            description: "create_messages_senders",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "create_messages_table",
+            sql: "CREATE TABLE IF NOT EXISTS messages (id TEXT PRIMARY KEY, chatroom_id INTEGER, content TEXT, created_at TEXT, sender_id INTEGER, FOREIGN KEY(sender_id) REFERENCES senders(id));",
+            kind: MigrationKind::Up,
+        }
+    ];
+
     tauri::Builder::default()
-        // 2. Registra o estado para gerenciar as conexões.
-        //    Isso disponibiliza o `ConnectionManager` para seus comandos.
-        .manage(Mutex::new(HashMap::new()) as commands::ConnectionManager)
-                .plugin(tauri_plugin_store::Builder::new().build()) // <-- ADICIONE ESTA LINHAS
-        .plugin(tauri_plugin_opener::init()) // Seu plugin existente
-        
-        // 3. Registra TODOS os comandos necessários.
+        .manage(commands::WsState(Arc::new(Mutex::new(None))))
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:mydatabase.db", migrations)
+                .build(),
+        )
+        .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                let app_dir = app_handle
+                    .path()
+                    .app_data_dir()
+                    .expect("Failed to get app data dir");
+                let db_path = app_dir.join("mydatabase.db");
+                let pool = SqlitePool::connect(&format!("sqlite:{}", db_path.to_str().unwrap()))
+                    .await
+                    .expect("Failed to connect to database");
+                app_handle.manage(pool);
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
-            commands::connect_chat,
-            commands::disconnect_chat
+            commands::subscribe_to_channel,
+            commands::unsubscribe_from_channel,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
