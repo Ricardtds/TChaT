2878a7b7856242e916842a0a08cc90787b770fc6
Ricardo Teixeira dos Santos <ricardoteixeirasantos@hotmail.com>
2025-08-30 13:20:34 -0300

    fix: adjusting sqlx struct

--- src-tauri/src/models/db.rs
+++ src-tauri/src/models/db.rs
@@ -1,16 +1,18 @@
-#[derive(Debug)]
-pub struct Sender {
-    pub id: u64,
-    pub username: String,
-    pub slug: String,
-    pub color: String,
-}
+use serde::Serialize;
+use sqlx::FromRow;
 
-#[derive(Debug)]
-pub struct ChatMessage {
-    pub id: String,
-    pub chatroom_id: u64,
+#[derive(FromRow, Serialize)]
+
+pub struct MessageWithSender {
+    // Fields from the 'messages' table
+    pub message_id: String,
+    pub chatroom_id: i64,
     pub content: Option<String>,
     pub created_at: String,
-    pub sender_id: u64
+
+    // Fields from the 'senders' table
+    pub sender_id: i64,
+    pub username: Option<String>,
+    pub slug: Option<String>,
+    pub color: Option<String>,
 }
